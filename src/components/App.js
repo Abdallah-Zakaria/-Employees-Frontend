ddc0f21cad53a7a0041656e5041d7bedf2ec6351
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2021-09-17 17:57:09 +0300

    header

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import Header from "./Header";
 import DataTable from "./DataTable";
 import Footer from "./Footer";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div >
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,35 @@
+import { Navbar, Container } from 'react-bootstrap';
+
 import './index.scss'
 
 function Header() {
   return (
-    <div >
+    <div>
+      <Navbar >
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/pwc.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <span style={{ fontWeight: 'bold', fontFamily: 'fantasy', color: '#464646' }}>Employees</span>
+          </Navbar.Brand>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <span style={{ fontWeight: 'bold', color: '#464646', marginRight: '8px' }}> Abdallah Zakaria</span>
+            <img
+              alt=""
+              src="/abdallah-zakaria.jpg"
+              className="d-inline-block align-top"
+              width='36px'
+              height='36px'
+              style={{ objectFit: 'cover', borderRadius: '50%' }}
+            />
+          </div>
+        </Container>
+      </Navbar>
     </div>
   );
 }
